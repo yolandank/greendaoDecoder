c541e8886e5dbef48b31e6390eee07ad539121ab
任豹 <wejie.rwj@agtech.com>
2020-03-20 15:53:42 +0800

    1. 完成Glide源码阅读 2. 添加别人的线程池工具类

--- app/src/main/java/com/example/greendaodecoder/MyAppGlideModule.java
+++ app/src/main/java/com/example/greendaodecoder/MyAppGlideModule.java
@@ -16,6 +16,10 @@ public class MyAppGlideModule extends AppGlideModule {
     @Override
     public void applyOptions(Context context, GlideBuilder builder) {
         // 实际使用中根据情况可以添加如下配置
+        // GlideApp的作用：
+        // 1. 初始化各式各样的配置信息（包括缓存，请求线程池，大小，图片格式等等）
+        // 2. 初始化glide对象。
+
         builder.setDefaultRequestOptions(new RequestOptions().format(DecodeFormat.PREFER_RGB_565));
         int memoryCacheSizeBytes = 1024 * 1024 * 20;
         builder.setMemoryCache(new LruResourceCache(memoryCacheSizeBytes));
